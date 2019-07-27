4676494864ca53a0b01cf39613cb178862a19dfd
DenisHubLine <53374039+DenisHubLine@users.noreply.github.com>
2019-07-27 22:24:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
     <html>
     <head>
-    <!-- Подключаем jquery -->
-    <script src='http://code.jquery.com/jquery-1.7.1.js'></script>
-    <!-- Подключаем VK-->
-    <script src="http://vk.com/js/api/xd_connection.js?2" type="text/javascript"></script>
-    <!-- Подключаем скрипт регулировки высоты-->
+
     </head>
     <body style='text-align:center;padding-top:50px;' id='body0'>
     Hello, world!
