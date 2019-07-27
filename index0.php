45fd49728ca6cd1f45d9a1cee950f54d4f72e3e3
DenisHubLine <53374039+DenisHubLine@users.noreply.github.com>
2019-07-27 21:38:39 +0300

    Update index0.php

--- index0.php
+++ index0.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Подключаем jquery -->
+<script src='http://code.jquery.com/jquery-1.7.1.js'></script>
+</head>
+<!-- ОБЯЗАТЕЛЬНО указываем <body> id='body' !-->
+<body style='text-align:center;padding-top:50px;'>
+</body>
+</html>
